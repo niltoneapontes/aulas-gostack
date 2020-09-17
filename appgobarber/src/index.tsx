d4c6a35fa7b6224fcdeafd245ec998622d0e8e48
Nilton Pontes <niltoneapontes@gmail.com>
2020-09-16 23:12:57 -0300

    Add novas aulas

---
+++ appgobarber/src/index.tsx
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { View, StatusBar, Text, StyleSheet } from 'react-native';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
+
+export default App;
