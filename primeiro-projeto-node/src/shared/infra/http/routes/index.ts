c43df5a626eb3c10a671902f18c121685a47088a
Nilton Pontes <niltoneapontes@gmail.com>
2020-09-28 02:00:51 -0300

    Adicionando Update do Profile

--- primeiro-projeto-node/src/shared/infra/http/routes/index.ts
+++ primeiro-projeto-node/src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from '@modules/Users/infra/http/routes/users.routes';
 import appointmentsRouter from '@modules/Appointments/infra/http/routes/appointments.routes';
 import sessionsRouter from '@modules/Users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/Users/infra/http/routes/password.routes';
+import profileRouter from '@modules/Users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use('/appointments', appointmentsRouter); // o "use" irá funcionar para 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
